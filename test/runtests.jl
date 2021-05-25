d9b5f37395fa28a36a604257396383c212cb1b22
Roger-Luo <rogerluo.rl18@gmail.com>
2021-05-25 15:29:17 -0400

    test more

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
 using ComoniconTestUtils
 using ComoniconZSHCompletion: emit_zshcompletion
 using Test
+using Random
 
-@testset "test completion" begin
+Random.seed!(42)
+
+@testset "test completion" for _ in 1:5
     cmd = rand_command()
     script = emit_zshcompletion(cmd)
     @test occursin("#compdef _$(cmd.root.name) $(cmd.root.name)", script)
